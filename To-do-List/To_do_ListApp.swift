ad8910d292c3fb263ab5290fff1ad65561f4ce82
meetsuhagiya <meetsuhagiya420@gmail.com>
2025-03-28 15:21:49 +0530

    Initial Commit

---
+++ To-do-List/To_do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_ListApp.swift
+//  To-do-List
+//
+//  Created by student on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct To_do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
